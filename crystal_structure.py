596060dec0083652721ca2799061a3a77e94f59b
shenbeichen <shenbeichen@gmail.com>
2019-03-09 22:28:22 +0800

    Update the method of reading, converting and writing outfiles.

--- crystal_structure.py
+++ crystal_structure.py
@@ -1,52 +1,54 @@
 #!/usr/bin/env python3
 import numpy as np
-from numpy import linalg as LA
+from numpy.linalg import norm, det, inv
 
 class crystal_structure:
 
 
-    def __init__ ( self, filename ):
-        self.filename = filename
-        with open( self.filename ) as f:
-            lines = f.read().splitlines()
-        # Read and parse the input
-        self.a = [float(i) for i in lines[0].split(' ')]
-        self.b = [float(i) for i in lines[1].split(' ')]
-        self.c = [float(i) for i in lines[2].split(' ')]
-        self.u = [float(i) for i in lines[3].split(' ')]
-        self.v = [float(i) for i in lines[4].split(' ')]
-
-        self.alpha = np.arccos(np.dot(self.b,self.c)/(LA.norm(self.b)*LA.norm(self.c)))
-        self.beta  = np.arccos(np.dot(self.a,self.c)/(LA.norm(self.a)*LA.norm(self.c)))
-        self.gamma = np.arccos(np.dot(self.a,self.b)/(LA.norm(self.a)*LA.norm(self.b)))
+    def __init__ (self, filename):
+        self.fname = filename
+        self.crystal_info = np.loadtxt(self.fname)
+        self.check()
         
-        self.a_ = 2*np.pi*np.cross(self.b,self.c)/np.dot(self.a,np.cross(self.b,self.c))
-        self.b_ = 2*np.pi*np.cross(self.c,self.a)/np.dot(self.b,np.cross(self.c,self.a))
-        self.c_ = 2*np.pi*np.cross(self.a,self.b)/np.dot(self.c,np.cross(self.a,self.b))
-        self.alpha_ = np.arccos((np.cos(self.beta)*np.cos(self.gamma)-np.cos(self.alpha))/(np.sin(self.beta)*np.sin(self.gamma)))
-        self.beta_  = np.arccos((np.cos(self.alpha)*np.cos(self.gamma)-np.cos(self.beta))/(np.sin(self.alpha)*np.sin(self.gamma)))
-        self.gamma_ = np.arccos((np.cos(self.alpha)*np.cos(self.beta)-np.cos(self.gamma))/(np.sin(self.alpha)*np.sin(self.beta)))
-
+        if self.crystal_info.shape[0] == 4:
+            self.alatt = self.crystal_info[0]
+            self.ang = np.deg2rad(self.crystal_info[1])
+            self.u = self.crystal_info[2]
+            self.v = self.crystal_info[3]
+        
+        sin_ang, cos_ang = np.sin(self.ang), np.cos(self.ang)
+        V = np.sqrt(np.abs(1 - np.sum(cos_ang**2) + 2 * np.prod(cos_ang)))
+        
+        self.alatt_ = 2 * np.pi * sin_ang / self.alatt / V
+        self.ang_ = np.arccos( (np.roll(cos_ang, 1) * np.roll(cos_ang, 2) - cos_ang) / \
+                    (np.roll(sin_ang, 1) * np.roll(sin_ang, 2)) )     
     
-    def get_U_matrix( self, psi):
-        t1 = self.u / LA.norm(self.u)
-        t2 = self.v / LA.norm(self.v)
-        t3 = np.cross(t1,t2)
-        U_mat = [[t2[0]*np.cos(psi)  ,t1[0]*np.sin(psi) ,0.       ],
-                 [0.                 ,0.                , t3[2]*1.],
-                 [-t2[2]*np.sin(psi) ,t1[2]*np.cos(psi) ,0.       ]]
-        return U_mat
+    def check(self):
+        assert (self.crystal_info.shape[1] == 3), "Error: Wrong crystal info format!"
+        if np.any(self.crystal_info[0]<=0):
+            raise ValueError("Lattice constants should be positive!")
+        if np.any(self.crystal_info[1] <= 0) or np.any(self.crystal_info[1] >= 180):
+            raise ValueError("Lattice angles should be in the range (0, 180) deg!")
+        return None
 
-    def get_B_matrix( self ):
-        B_mat = [[LA.norm(self.a_), LA.norm(self.b_)*np.cos(self.gamma_), LA.norm(self.c_)*np.cos(self.beta_)],
-                 [0.              , LA.norm(self.b_)*np.sin(self.gamma_), -LA.norm(self.c_)*np.sin(self.beta_)*np.cos(self.alpha)],
-                 [0.              , 0.                                  ,1/LA.norm(self.c)]]
-        return B_mat
-
-    def get_inverse( self, mat ):
-        return LA.inv(mat)
-
-
+    def get_UB_matrix_inv(self):
+        a_, b_, c_ = self.alatt_
+        alpha_, beta_, gamma_ = self.ang_
+        self.B = np.array([[a_, b_ * np.cos(gamma_),  c_ * np.cos(beta_)], \
+                           [ 0, b_ * np.sin(gamma_), -c_ * np.sin(beta_) * np.cos(self.ang[0]) ], \
+                           [ 0,                   0,  2 * np.pi / self.alatt[-1]]])
+        
+        u_col, v_col = np.expand_dims(self.u, 1), np.expand_dims(self.v, 1)
+        u_c, v_c = np.squeeze(np.matmul(self.B, [u_col, v_col]))
+        
+        e1 = u_c / norm(u_c)
+        e3 = np.cross(u_c, v_c) / norm(np.cross(u_c, v_c))
+        e2 = np.cross(e3, e1)
+        
+        U = np.vstack((e1, e2, e3))
+        self.U = U / det(U)
+        
+        return inv(np.matmul(self.U, self.B))
 
 
 
