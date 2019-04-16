5af7a761fba1fcc831c7be2370fcdb35f5aed5a8
shenbeichen <shenbeichen@gmail.com>
2019-04-16 13:03:31 -0700

    Use the lambda function suggested by Claire.

--- src/all_nxspe.py
+++ src/all_nxspe.py
@@ -6,10 +6,9 @@ import h5py as h5
 import numpy as np
 from numpy.linalg import inv
 import time
+from math import sqrt
 
-
-def get_k(energy, e_to_k = 0.694692092176654):
-    return np.sqrt(energy) * e_to_k
+get_k = lambda energy : sqrt( energy ) * 0.694692092176654
 
 class nxspe:
     def __init__(self, infile_name, override_psi=0, ein=0):
@@ -49,13 +48,11 @@ class nxspe:
     
         k_i = get_k(self.incident_energy)
         sin_psi, cos_psi = np.sin(self.psi), np.cos(self.psi)
-        print( sin_psi, sin_cos)
-        spec_to_uv = np.array([[ cos_psi, sin_psi, 0], \
-                               [-sin_psi, cos_psi, 0], \
-                               [       0,       0, 1]])
-        print ( type(spec_to_uv) )
-        
-        spec_to_rlu = np.matmul(inv_UB, spec_to_uv)
+        self.sepc_to_uv = np.array([[ cos_psi, sin_psi, 0], \
+                                    [-sin_psi, cos_psi, 0], \
+                                    [       0,       0, 1]])
+        # uv_to_rlu = inv_UB
+        spec_to_rlu = np.matmul(inv_UB, self.sepc_to_uv)
         
         if not throw_nan and not keep_zeros:
             raise ValueError("Do not keep nan value while removing zeros!")
@@ -98,99 +95,99 @@ class nxspe:
                 np.savetxt(f, np.hstack((Q_in_rlu, E_col, I_col)), fmt='%.4f,%.4f,%.4f,%.2f,%.4e')
 
 
-#def check_nxspe_consistency(n1, n2):
-#
-#    assert n1.incident_energy == n2.incident_energy, "Incident energy does not match!"
-#    assert np.allclose(n1.azimuthal, n2.azimuthal) and np.allclose(n1.polar, n2.polar), "Geometry does not match!"
-#    assert np.allclose(n1.ph_energy_boundries, n2.ph_energy_boundries), "Energy bins do not match!"
-#    assert np.all(n1.masked_region == n2.masked_region), "Masked detectors do not match!"
-#
-#    return None
-
-#def get_bg_nxspe(bg_data_files, ein):
-#
-#    bg_n = nxspe(bg_data_files[0], ein=ein)
-#
-#    for fname in bg_data_files[1:]:
-#        tmp_n = nxspe(fname, ein=ein)
-#        check_nxspe_consistency(bg_n, tmp_n)
-#        bg_n.intensity += tmp_n.intensity
-#    bg_n.intensity /= len(bg_data_files)
-#
-#    return bg_n
-#
-#class nxspe_with_bg:
-#
-#    def __init__(self, sc_fname, bg_data, override_psi=0, ein=0):
-#        self.sc_n = nxspe(sc_fname, override_psi, ein)
-#        self.bg_n = bg_data
-#        check_nxspe_consistency(self.sc_n, self.bg_n)
-#        self.incident_energy = self.sc_n.incident_energy
-#        if not override_psi:
-#            self.psi = self.sc_n.psi
-#        else:
-#            self.psi = np.deg2rad(override_psi)
-#        self.azimuthal = self.sc_n.azimuthal
-#        self.polar = self.sc_n.polar
-#        self.ne = self.sc_n.ne
-#        self.ph_energy_centers = self.sc_n.ph_energy_centers
-#
-#        sin_psi, cos_psi = np.sin(self.psi), np.cos(self.psi)
-#        self.sepc_to_uv = np.array([[ cos_psi, sin_psi, 0], \
-#                                    [-sin_psi, cos_psi, 0], \
-#                                    [       0,       0, 1]])
-#
-#    def write_to_csv_while_converting( self, inv_UB, outdir='./', \
-#                                      throw_nan=True, keep_zeros=True, W=np.eye(3)):
-#
-#        if not throw_nan and not keep_zeros:
-#            raise ValueError("Do not keep nan value while removing zeros!")
-#
-#        k_i = get_k(self.incident_energy)
-#
-#        inv_W = inv(W)
-#
-#        # uv_to_rlu = inv_UB
-#        spec_to_rlu = np.matmul(inv_UB, self.sepc_to_uv)
-#
-#        if not throw_nan and not keep_zeros:
-#            raise ValueError("Do not keep nan value while removing zeros!")
-#
-#        for i in range(self.ne):
-#            k_f = get_k(self.incident_energy-self.ph_energy_centers[i])
-#
-#            if throw_nan:
-#                if keep_zeros:
-#                    ix_keep = np.isfinite(self.sc_n.intensity[:, i])
-#                else:
-#                    ix_keep = np.nonzero(np.nan_to_num(self.sc_n.intensity[:, i]))
-#                theta, phi = self.polar[ix_keep], self.azimuthal[ix_keep]
-#                sc_I_col = np.expand_dims(self.sc_n.intensity[:, i][ix_keep], axis=1)
-#                bg_I_col = np.expand_dims(self.bg_n.intensity[:, i][ix_keep], axis=1)
-#            else:
-#                # This will not waste time, they are not np.copy()
-#                theta, phi = self.polar, self.azimuthal
-#                sc_I_col = np.expand_dims(self.sc_n.intensity[:, i], axis=1)
-#                bg_I_col = np.expand_dims(self.bg_n.intensity[:, i], axis=1)
-#
-#            sin_theta = np.sin(theta)
-#            ex = np.cos(theta)
-#            ey = sin_theta * np.cos(phi)
-#            ez = sin_theta * np.sin(phi)
-#
-#            q1 = k_i - ex * k_f
-#            q2 = -ey * k_f
-#            q3 = -ez * k_f
-#
-#            Q_in_rlu = np.matmul( spec_to_rlu, np.vstack((q1, q2, q3)) ).T
-#
-#            if not np.allclose(W, np.eye(3)):
-#                inv_W = inv(W)
-#                Q_in_rlu = np.matmul(inv_W, Q_in_rlu.T).T
-#
-#            E_col = np.full( (Q_in_rlu.shape[0], 1), self.ph_energy_centers[i] )
-#
-#            fname = outdir + ('QEI_no_nan_with_bg.csv' if keep_zeros else 'QEI_no_nan_no_zero_with_bg.csv')
-#
-#            with open(fname, 'ab+') as f:
-#                np.savetxt(f, np.hstack((Q_in_rlu, E_col, sc_I_col, bg_I_col)), fmt='%.4f,%.4f,%.4f,%.2f,%.4e,%.4e')
+def check_nxspe_consistency(n1, n2):
+    
+    assert n1.incident_energy == n2.incident_energy, "Incident energy does not match!"
+    assert np.allclose(n1.azimuthal, n2.azimuthal) and np.allclose(n1.polar, n2.polar), "Geometry does not match!"
+    assert np.allclose(n1.ph_energy_boundries, n2.ph_energy_boundries), "Energy bins do not match!"
+    assert np.all(n1.masked_region == n2.masked_region), "Masked detectors do not match!"
+    
+    return None
+            
+def get_bg_nxspe(bg_data_files, ein):
+    
+    bg_n = nxspe(bg_data_files[0], ein=ein)
+    
+    for fname in bg_data_files[1:]:
+        tmp_n = nxspe(fname, ein=ein)
+        check_nxspe_consistency(bg_n, tmp_n)
+        bg_n.intensity += tmp_n.intensity
+    bg_n.intensity /= len(bg_data_files)
+    
+    return bg_n
+        
+class nxspe_with_bg:
+    
+    def __init__(self, sc_fname, bg_data, override_psi=0, ein=0):
+        self.sc_n = nxspe(sc_fname, override_psi, ein)
+        self.bg_n = bg_data
+        check_nxspe_consistency(self.sc_n, self.bg_n)
+        self.incident_energy = self.sc_n.incident_energy
+        if not override_psi:
+            self.psi = self.sc_n.psi
+        else:
+            self.psi = np.deg2rad(override_psi)
+        self.azimuthal = self.sc_n.azimuthal
+        self.polar = self.sc_n.polar
+        self.ne = self.sc_n.ne
+        self.ph_energy_centers = self.sc_n.ph_energy_centers
+        
+        sin_psi, cos_psi = np.sin(self.psi), np.cos(self.psi)
+        self.sepc_to_uv = np.array([[ cos_psi, sin_psi, 0], \
+                                    [-sin_psi, cos_psi, 0], \
+                                    [       0,       0, 1]])
+    
+    def write_to_csv_while_converting( self, inv_UB, outdir='./', \
+                                      throw_nan=True, keep_zeros=True, W=np.eye(3)): 
+        
+        if not throw_nan and not keep_zeros:
+            raise ValueError("Do not keep nan value while removing zeros!")
+            
+        k_i = get_k(self.incident_energy)
+        
+        inv_W = inv(W)
+        
+        # uv_to_rlu = inv_UB
+        spec_to_rlu = np.matmul(inv_UB, self.sepc_to_uv)
+        
+        if not throw_nan and not keep_zeros:
+            raise ValueError("Do not keep nan value while removing zeros!")
+        
+        for i in range(self.ne):
+            k_f = get_k(self.incident_energy-self.ph_energy_centers[i])
+            
+            if throw_nan:
+                if keep_zeros:
+                    ix_keep = np.isfinite(self.sc_n.intensity[:, i])
+                else:
+                    ix_keep = np.nonzero(np.nan_to_num(self.sc_n.intensity[:, i]))
+                theta, phi = self.polar[ix_keep], self.azimuthal[ix_keep]
+                sc_I_col = np.expand_dims(self.sc_n.intensity[:, i][ix_keep], axis=1)
+                bg_I_col = np.expand_dims(self.bg_n.intensity[:, i][ix_keep], axis=1)
+            else:
+                # This will not waste time, they are not np.copy()
+                theta, phi = self.polar, self.azimuthal
+                sc_I_col = np.expand_dims(self.sc_n.intensity[:, i], axis=1)
+                bg_I_col = np.expand_dims(self.bg_n.intensity[:, i], axis=1)
+            
+            sin_theta = np.sin(theta)
+            ex = np.cos(theta)
+            ey = sin_theta * np.cos(phi)
+            ez = sin_theta * np.sin(phi)
+            
+            q1 = k_i - ex * k_f
+            q2 = -ey * k_f
+            q3 = -ez * k_f
+                
+            Q_in_rlu = np.matmul( spec_to_rlu, np.vstack((q1, q2, q3)) ).T
+            
+            if not np.allclose(W, np.eye(3)):
+                inv_W = inv(W)
+                Q_in_rlu = np.matmul(inv_W, Q_in_rlu.T).T
+                
+            E_col = np.full( (Q_in_rlu.shape[0], 1), self.ph_energy_centers[i] )
+         
+            fname = outdir + ('QEI_no_nan_with_bg.csv' if keep_zeros else 'QEI_no_nan_no_zero_with_bg.csv')
+            
+            with open(fname, 'ab+') as f:
+                np.savetxt(f, np.hstack((Q_in_rlu, E_col, sc_I_col, bg_I_col)), fmt='%.4f,%.4f,%.4f,%.2f,%.4e,%.4e')
