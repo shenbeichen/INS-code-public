f04b7e862c19a205d8262c13efe13f2d203d55d4
shenbeichen <shenbeichen@gmail.com>
2019-04-16 13:12:09 -0700

    Remove the meaningless time part.

--- src/correct_q_offset.py
+++ src/correct_q_offset.py
@@ -4,7 +4,7 @@
 import time
 
 def correct_q_offset(df, T):
-    t1 = time.time()
+    #t1 = time.time()
     print("Start: q-offset correction...")
     corrected_df = df.withColumn("H_new", T[0, 0] * df.H + T[0, 1] * df.K + T[0, 2] * df.L + T[0, 3])\
                      .withColumn("K_new", T[1, 0] * df.H + T[1, 1] * df.K + T[1, 2] * df.L + T[1, 3])\
@@ -13,10 +13,6 @@ def correct_q_offset(df, T):
                      .withColumnRenamed("H_new", "H")\
                      .withColumnRenamed("K_new", "K")\
                      .withColumnRenamed("L_new", "L")
-    t2 = time.time()
-    print("Finish: q-offset correction. Time used: {:.2f} seconds.".format(t2-t1))
+    #t2 = time.time()
+    #print("Finish: q-offset correction. Time used: {:.2f} seconds.".format(t2-t1))
     return corrected_df
-
-
-
-
